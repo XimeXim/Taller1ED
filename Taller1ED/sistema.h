edc48c26c4964889c24a133ced943a8441493620
ThomasAlmunaPrieto <thomas.almuna@alumnos.ucn.cl>
2024-04-24 23:29:32 -0400

    ultimo commit de documentacion mio

--- Taller1ED/sistema.h
+++ Taller1ED/sistema.h
@@ -19,11 +19,40 @@ private:
     Estanteria estanteria;
 
 public:
+	/**
+	 * Constructor del sistema
+	 */
 	Sistema();
-	void lecturaArchivos();
+
+    /**
+     * Metodo que se encarga de la lectura de archivos
+     */
+    void lecturaArchivos();
+
+	/**
+	 * Metodo que cambia parámetros de string a bool
+	 * @param str String que cambiará a bool
+	 * @return El bool que antes era un string
+	 */
 	bool stringToBool(string str);
-	void actualizarArchivos();
+
+    /**
+     * Metodo que actualiza el archivo
+     */
+    void actualizarArchivos();
+
+    /**
+     * Metodo que contiene el menú principal
+     */
     void MenuPrincipal();
+
+    /**
+     * Metodo que contiene el menu de estadísticas
+     */
     void MenuEstadisticas();
-	~Sistema();
+
+    /**
+     * Destructor del sistema
+     */
+    ~Sistema();
 };
